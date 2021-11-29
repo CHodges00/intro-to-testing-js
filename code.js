7f51370f5c85ff21942bdaedf81ba8525c5e154a
Cody Hodges <hodges.cody00@gmail.com>
2021-11-29 17:01:41 -0600

    unit tests for edge cases

--- code.js
+++ code.js
@@ -9,6 +9,16 @@
 // }
 
 function sayHello(name) {
+    if (typeof(name) === "undefined"){
+        return 'Hello, World!';
+    }else if (typeof(name) === "boolean") {
+        return 'Hello, World!'
+    }else if (typeof(name) === "number") {
+        return 'That\'s a number !';
+    }else{
+        return `Hello, ${name}!`;
+    }
+}
 //     if (name === 'Alex') {
 //         return ('Hello, Alex!');
 //     }else if (name === 'Pat'){
@@ -17,6 +27,5 @@ function sayHello(name) {
 //         return ('Hello, Jane!')
 //     }
 // }
-    return ('Hello, ' + name + '!');
-}
+
 
--- tests.js
+++ tests.js
@@ -30,5 +30,17 @@ describe('sayHello', function (){
     it('should return the string "Hello, Pat!" when executed', function (){
        expect(sayHello('Pat')).toBe('Hello, Pat!');
     });
+    it('should return the string "Hello, World!" when executed', function (){
+        expect(sayHello()).toBe('Hello, World!');
+    });
+    it('should return the string "Hello, World!" when executed', function (){
+        expect(sayHello(false)).toBe('Hello, World!')
+    });
+    it('should return the string "Hello, World!" when executed', function (){
+        expect(sayHello(true)).toBe('Hello, World!')
+    });
+    it('should return the string "That\'s a number !" when executed', function (){
+        expect(sayHello(40)).toBe('That\'s a number !')
+    });
 });
 
