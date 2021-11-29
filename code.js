dddd010b924b3fd1dc907c0e34f664f9016e380c
Cody Hodges <hodges.cody00@gmail.com>
2021-11-29 16:18:39 -0600

    add tests and ability to say 'Hello' to any input.

--- code.js
+++ code.js
@@ -8,6 +8,15 @@
 //     return "Hello, World!";
 // }
 
-function sayHello(name){
-    return (name);
+function sayHello(name) {
+//     if (name === 'Alex') {
+//         return ('Hello, Alex!');
+//     }else if (name === 'Pat'){
+//         return ('Hello, Pat!')
+//     }else{
+//         return ('Hello, Jane!')
+//     }
+// }
+    return ('Hello, ' + name + '!');
 }
+
